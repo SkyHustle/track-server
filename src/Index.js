4914b17416e213e858ff2ee1de34c3ccdc868680
Dmitry Sky <dmitry.sky@icloud.com>
2020-01-01 14:20:54 +1000

    configure and connect mongoDB cluster, add some logic for handling connection and errors.

--- src/Index.js
+++ src/Index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const mongoUri = require('../mongoUri');
 
 const app = express();
+
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi There!');
